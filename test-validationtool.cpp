cb03ab09d35cc4d85e0c606ca39fddbdff7047ac
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-03-24 00:39:09 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -20,5 +20,5 @@ TEST_CASE("reports error when current jumps abruptly") {
 TEST_CASE("reports NAN when readings are empty") {
     double socReadings[] = {};
     int numOfSocReadings = 0;
-    REQUIRE(isnan(validateSOCreadings(0, 0)));
+    REQUIRE(isnan(currentReadings, numOfCurReadings));
 }
