ba9051e8589cbdcd8450d97cacea57c22e36cac1
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-03-24 19:16:34 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,15 +1,19 @@
-#include "sensor-validate.h"
-
+#include "sensor_validate.h"
+#include <stdlib.h>
+#include <math.h>
 
 int IsDifference_GreaterthanThreshold(double value, double nextValue, double maxDelta) {
- if(nextValue - value > maxDelta) {
+  if(nextValue - value > maxDelta) {
     return 0;
   }
   return 1;
 }
 
-int validateParameterreadings(double* values, int numOfValues,double maxDelta) {
-   
+float validateParameterreadings(double* values, int numOfValues,double maxDelta) {
+    if(numOfValues == 0 || values == NULL)
+    {
+        return NAN;
+    }
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
     if(!IsDifference_GreaterthanThreshold(values[i], values[i + 1],maxDelta)) {
@@ -19,11 +23,13 @@ int validateParameterreadings(double* values, int numOfValues,double maxDelta) {
   return 1;
 }
 
-int validateSOCreadings(double* values, int numOfValues) {
-    return(validateParameterreadings(values,numOfValues,0.05));
+float validateSOCreadings(double* values, int numOfValues) {
+   return(validateParameterreadings(values,numOfValues,0.05));
+    
     }
 
-int validateCurrentreadings(double* values, int numOfValues) {
+
+float validateCurrentreadings(double* values, int numOfValues) {
     return(validateParameterreadings(values,numOfValues,0.1));
 }
 
