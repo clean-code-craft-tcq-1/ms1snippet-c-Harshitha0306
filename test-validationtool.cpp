f76991d68e62eb17c65642ae2ae5ab8fb2b5ca3b
Harshitha0306 <77484111+Harshitha0306@users.noreply.github.com>
2021-03-25 00:25:06 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -1,3 +1,15 @@
+/*
+ ***************************************************************************************************
+ * Author:             Harshitha Subramani
+ * Date:               24/03/2021
+ * Filename:           test-validation.cpp
+ *****************************************************************************************************
+*
+***************************************************************************************
+* Includes
+***************************************************************************************
+*/
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
@@ -5,11 +17,23 @@
 #include "math.h"
 
 
+/*
+***************************************************************************************
+* Testcase to validate noise in SOC sensor readings
+***************************************************************************************
+*/
+
 TEST_CASE("reports error when soc jumps abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
   REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0);
 }
+
+/*
+***************************************************************************************
+* Testcase to validate noise in Current sensor readings
+***************************************************************************************
+*/
 
 TEST_CASE("reports error when current jumps abruptly") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
@@ -17,9 +41,28 @@ TEST_CASE("reports error when current jumps abruptly") {
   REQUIRE(validateCurrentreadings(currentReadings, numOfCurReadings) == 0);
 }
 
-TEST_CASE("reports NAN when readings are empty") {
+/*
+***************************************************************************************
+* Testcase to validate  whether the SOC sensor readings are empty
+***************************************************************************************
+*/
+
+TEST_CASE("reports NAN when readings are empty for SOC Readings") {
     double socReadings[] = {};
     int numOfSocReadings = sizeof(socReadings) / sizeof(double);
     float a = validateSOCreadings(socReadings, numOfSocReadings);
-  REQUIRE(isnan(a));
+    REQUIRE(isnan(a));
+}
+
+/*
+***************************************************************************************
+* Testcase to validate  whether the Current sensor readings are empty
+***************************************************************************************
+*/
+
+TEST_CASE("reports NAN when readings are empty for Current Readings") {
+    double currentReadings[] = {};
+    int numOfCurReadings = sizeof(currentReadings) / sizeof(double);
+    float a = validateCurrentreadings(currentReadings, numOfCurReadings);
+    REQUIRE(isnan(a));
 }
